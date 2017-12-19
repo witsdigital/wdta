d96ba695e7f1ae6da398a286ecb6f3ae14bbb04d
witsdigital <34650770+witsdigital@users.noreply.github.com>
2017-12-19 20:00:55 -0300

    update v 1.0.6
    back end das patologias feito

--- src/pages/detalhe-consulta/detalhe-consulta.ts
+++ src/pages/detalhe-consulta/detalhe-consulta.ts
@@ -1,3 +1,4 @@
+import { ServiceProvider } from './../../providers/service/service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -16,20 +17,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class DetalheConsultaPage {
 
   sintomas: any;
+  allsintomas: any;
   patologia: any
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public service: ServiceProvider, public navCtrl: NavController, public navParams: NavParams) {
     this.sintomas = navParams.get("sin");
     this.patologia = navParams.get("pat");
-
+    this. getSintomas();
     console.log(this.patologia);
     console.log(this.sintomas);
+    
   
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetalheConsultaPage');
   }
+
+  getSintomas(){
+    
+        this.service.getSintomasID(this.patologia).then((data)=>{
+         this.allsintomas = data;
+         console.log(this.allsintomas);
+            },(err)=>{
+        
+            });
+        
+    }
 
 
 }
