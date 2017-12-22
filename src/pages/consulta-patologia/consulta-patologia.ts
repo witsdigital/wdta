8f9aca746cffe9a0c712e9fd32caf1e549b24eea
witsdigital <thiago.89@icloud.com>
2017-12-22 18:24:22 -0200

    atualização de interface

--- src/pages/consulta-patologia/consulta-patologia.ts
+++ src/pages/consulta-patologia/consulta-patologia.ts
@@ -1,7 +1,7 @@
 import { DetalhePatologiaPage } from './../detalhe-patologia/detalhe-patologia';
 import { ServiceProvider } from './../../providers/service/service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the ConsultaPatologiaPage page.
@@ -16,33 +16,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'consulta-patologia.html',
 })
 export class ConsultaPatologiaPage {
-  
+
     sintomas: any =[];
-  
+
     tem:any;
-  
+
     lista:any[];
     items: any[];
-  
-    constructor(public service: ServiceProvider, public navCtrl: NavController, public navParams: NavParams) {
-    
+
+    constructor(public modalCtrl: ModalController, public service: ServiceProvider, public navCtrl: NavController, public navParams: NavParams) {
+
     this.getDados();
-    
+
     }
-  
+
     ionViewDidLoad() {
       console.log('ionViewDidLoad ConsultaPage');
     }
-  
-  
+
+
     initializeItems() {
-      
+
           this.items = this.lista;
-      
+
        }
-  
+
   getSintomas(ev: any) {
-    
+
   this.initializeItems();
       let val = ev.target.value;
       if (val && val.trim() != '') {
@@ -52,41 +52,48 @@ export class ConsultaPatologiaPage {
         })
       }else{
             this.tem = false;
-  
+
       }
     }
-  
-  
+
+
     getDados(){
-  
+
       this.service.getPat().subscribe((data)=>{
-      
+
        this.lista = data;
        console.log(this.lista);
        this.items = this.lista;
           },(err)=>{
-      
+
           });
-      
+
         }
-      
-  
+
+
       addSintomas(item) {
-  
+
         this.sintomas.push(item);
           console.log(this.sintomas);
       }
-  
+
       delete(item){
         const index = this.sintomas.indexOf(item);
         this.sintomas.splice(index, 1);
-  
+
      }
-  
+
      consultar(item){
-      this.navCtrl.push(DetalhePatologiaPage, {
-        item: item
-      }); 
+       console.log(item);
+
+       let modal = this.modalCtrl.create(DetalhePatologiaPage,{
+         item: item
+       });
+  modal.onDidDismiss(data => {
+
+
+  });
+  modal.present();
+
      }
   }
-  
--- src/pages/detalhe-patologia/detalhe-patologia.ts
+++ src/pages/detalhe-patologia/detalhe-patologia.ts
@@ -1,6 +1,6 @@
 import { ServiceProvider } from './../../providers/service/service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the DetalhePatologiaPage page.
@@ -15,66 +15,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'detalhe-patologia.html',
 })
 export class DetalhePatologiaPage {
-  
+
     sintomas: any;
     allsintomas: any;
     patologia: any;
     qtd=0;
     newsintomas:any = {nome:'', st:''};
     newsint:any = [];
-  
-    constructor(public service: ServiceProvider, public navCtrl: NavController, public navParams: NavParams) {
+
+    constructor(public service: ServiceProvider,public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams) {
       this.patologia = navParams.get("item");
+      console.log(this.patologia);
       this. getSintomas();
-  
-  
+
+
     }
-  
+
     ionViewDidLoad() {
       console.log('ionViewDidLoad DetalheConsultaPage');
     }
-  
+
+    close(){
+
+    this.viewCtrl.dismiss();
+  }
     getSintomas(){
-  
+
           this.service.getSintomasID(this.patologia).then((data)=>{
            this.allsintomas = data;
           for(let i in data){
             for(let a in this.sintomas){
               if(this.allsintomas[i].nome == this.sintomas[a].nome){
   this.newsint.push({nome:this.allsintomas[i].nome, st:1})
-  
-  
-  
+
+
+
             }else{
   this.newsint.push({nome:this.allsintomas[i].nome, st:0})
-  
+
             }
             }
-  
-  
+
+
           }
           console.log(this.newsint);
               },(err)=>{
-  
+
               });
-  
-  
-  
+
+
+
     }
     calcinfo(){
       for(let i=0; i<this.allsintomas.length-1; i++){
           for(let a = 0; a<this.sintomas.length-1;a++){
               if(this.allsintomas[i].nome == this.sintomas[a].nome){
                 console.log('esta');
-  
+
               }
-  
+
           }
-  
+
       }
     }
-  
-  
-  
+
+
+
   }
-  
